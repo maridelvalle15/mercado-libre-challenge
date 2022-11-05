9db467060a28a54d8fb322e334a68698864456ef
Marisela Del Valle <m.delvallem@uniandes.edu.co>
2022-11-04 22:16:34 -0500

    test fix add return to callback

--- client/config/app.js
+++ client/config/app.js
@@ -1,13 +1,13 @@
 import ApiLoader from "./boot-loaders/api.loader";
 import config from "./config";
 
-const loaders = [ApiLoader];
-
 export default class App {
   static boot() {
     this.config = config;
-    return Promise.all(loaders.map(s => s.boot(App)))
-      .then(() => true)
+    return Promise.resolve(ApiLoader.boot(App))
+      .then(() => {
+        console.info("Loaders done!");
+      })
       .catch(err => console.error("Error while booting the app", err));
   }
 }
